8047a62487c81801877fea0c40792494f16a25b2
brianxautumn <shred@brianautumn.com>
2018-10-25 20:07:22 -0700

    Added button handler

--- src/App.js
+++ src/App.js
@@ -5,15 +5,31 @@ import { connect } from 'react-redux';
 import { doTest } from './redux/actions';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: 'black',
+      banner: 'hello',
+      isOpen: false,
+    };
+    this.buttonHandler = this.buttonHandler.bind(this);
+  }
+
+  buttonHandler() {
+    console.log('my button is working')
+  }
+
   render() {
+    let myVariable = <h2>Brian</h2>;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {myVariable}
           </p>
-          <h2>413 Starter Kit </h2>
+          <h2>{this.state.banner}</h2>
+          <button onClick={this.buttonHandler} >Click Me</button>
         </header>
       </div>
     );
